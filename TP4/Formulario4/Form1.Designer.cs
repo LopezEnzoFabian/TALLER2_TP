ab982dd54a588cb3237d007a664382455c74f31b
Lopez Enzo Fabian <enzolopez789@gmail.com>
2024-08-27 23:59:08 -0300

    tp 4

---
+++ TP4/Formulario4/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace Formulario4
+{
+    partial class lblLista
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.lblDesde = new System.Windows.Forms.Label();
+            this.lblHasta = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtDesde = new System.Windows.Forms.TextBox();
+            this.txtHasta = new System.Windows.Forms.TextBox();
+            this.btnFuncion = new System.Windows.Forms.Button();
+            this.btnPares = new System.Windows.Forms.Button();
+            this.btnImpar = new System.Windows.Forms.Button();
+            this.btnPrimos = new System.Windows.Forms.Button();
+            this.eventLog1 = new System.Diagnostics.EventLog();
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(321, 60);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(180, 251);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // lblDesde
+            // 
+            this.lblDesde.AutoSize = true;
+            this.lblDesde.Location = new System.Drawing.Point(12, 60);
+            this.lblDesde.Name = "lblDesde";
+            this.lblDesde.Size = new System.Drawing.Size(38, 13);
+            this.lblDesde.TabIndex = 1;
+            this.lblDesde.Text = "Desde";
+            this.lblDesde.Click += new System.EventHandler(this.lblDesde_Click);
+            // 
+            // lblHasta
+            // 
+            this.lblHasta.AutoSize = true;
+            this.lblHasta.Location = new System.Drawing.Point(12, 118);
+            this.lblHasta.Name = "lblHasta";
+            this.lblHasta.Size = new System.Drawing.Size(35, 13);
+            this.lblHasta.TabIndex = 2;
+            this.lblHasta.Text = "Hasta";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(356, 32);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Lista de Numeros";
+            // 
+            // txtDesde
+            // 
+            this.txtDesde.Location = new System.Drawing.Point(56, 60);
+            this.txtDesde.Name = "txtDesde";
+            this.txtDesde.Size = new System.Drawing.Size(100, 20);
+            this.txtDesde.TabIndex = 4;
+            this.txtDesde.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // txtHasta
+            // 
+            this.txtHasta.Location = new System.Drawing.Point(53, 111);
+            this.txtHasta.Name = "txtHasta";
+            this.txtHasta.Size = new System.Drawing.Size(100, 20);
+            this.txtHasta.TabIndex = 5;
+            this.txtHasta.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // btnFuncion
+            // 
+            this.btnFuncion.Location = new System.Drawing.Point(187, 60);
+            this.btnFuncion.Name = "btnFuncion";
+            this.btnFuncion.Size = new System.Drawing.Size(100, 20);
+            this.btnFuncion.TabIndex = 6;
+            this.btnFuncion.Text = "Generar Funcion\r\n";
+            this.btnFuncion.UseVisualStyleBackColor = true;
+            this.btnFuncion.Click += new System.EventHandler(this.btnFuncion_Click);
+            // 
+            // btnPares
+            // 
+            this.btnPares.Location = new System.Drawing.Point(53, 161);
+            this.btnPares.Name = "btnPares";
+            this.btnPares.Size = new System.Drawing.Size(100, 29);
+            this.btnPares.TabIndex = 7;
+            this.btnPares.Text = "Numeros Pares";
+            this.btnPares.UseVisualStyleBackColor = true;
+            this.btnPares.Click += new System.EventHandler(this.btnPares_Click);
+            // 
+            // btnImpar
+            // 
+            this.btnImpar.Location = new System.Drawing.Point(56, 205);
+            this.btnImpar.Name = "btnImpar";
+            this.btnImpar.Size = new System.Drawing.Size(100, 31);
+            this.btnImpar.TabIndex = 8;
+            this.btnImpar.Text = "Numeros Impares";
+            this.btnImpar.UseVisualStyleBackColor = true;
+            this.btnImpar.Click += new System.EventHandler(this.btnImpar_Click);
+            // 
+            // btnPrimos
+            // 
+            this.btnPrimos.Location = new System.Drawing.Point(56, 253);
+            this.btnPrimos.Name = "btnPrimos";
+            this.btnPrimos.Size = new System.Drawing.Size(100, 29);
+            this.btnPrimos.TabIndex = 9;
+            this.btnPrimos.Text = "Numeros Primos";
+            this.btnPrimos.UseVisualStyleBackColor = true;
+            this.btnPrimos.Click += new System.EventHandler(this.btnPrimos_Click);
+            // 
+            // eventLog1
+            // 
+            this.eventLog1.SynchronizingObject = this;
+            // 
+            // lblLista
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(534, 342);
+            this.Controls.Add(this.btnPrimos);
+            this.Controls.Add(this.btnImpar);
+            this.Controls.Add(this.btnPares);
+            this.Controls.Add(this.btnFuncion);
+            this.Controls.Add(this.txtHasta);
+            this.Controls.Add(this.txtDesde);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblHasta);
+            this.Controls.Add(this.lblDesde);
+            this.Controls.Add(this.listBox1);
+            this.Name = "lblLista";
+            this.Text = "Formulario4";
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label lblDesde;
+        private System.Windows.Forms.Label lblHasta;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtDesde;
+        private System.Windows.Forms.TextBox txtHasta;
+        private System.Windows.Forms.Button btnFuncion;
+        private System.Windows.Forms.Button btnPares;
+        private System.Windows.Forms.Button btnImpar;
+        private System.Windows.Forms.Button btnPrimos;
+        private System.Diagnostics.EventLog eventLog1;
+    }
+}
+
---
+++ TP4/Formulario4/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Formulario4
+{
+    public partial class lblLista : Form
+    {
+        public lblLista()
+        {
+            InitializeComponent();
+        }
+
+        private void lblDesde_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+          
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+        
+        }
+
+        private void btnFuncion_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+
+            if (!int.TryParse(txtDesde.Text, out int start) || !int.TryParse(txtHasta.Text, out int end))
+            {
+                MessageBox.Show("Por favor, ingrese valores numéricos.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (start <= end)
+            {
+                // Imprimir en orden ascendente
+                for (int i = start; i <= end; i++)
+                {
+                    listBox1.Items.Add(i);
+                }
+            }
+            else
+            {
+                // Imprimir en orden descendente
+                for (int i = start; i >= end; i--)
+                {
+                    listBox1.Items.Add(i);
+                }
+            }
+         }
+
+        private void btnPares_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+
+            if (!int.TryParse(txtDesde.Text, out int start) || !int.TryParse(txtHasta.Text, out int end))
+            {
+                MessageBox.Show("Por favor, ingrese valores numéricos.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            for (int i = start; i <= end; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    listBox1.Items.Add(i);
+                }
+            }
+        }
+
+        private void btnImpar_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+
+            if (!int.TryParse(txtDesde.Text, out int start) || !int.TryParse(txtHasta.Text, out int end))
+            {
+                MessageBox.Show("Por favor, ingrese valores numéricos.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            for (int i = start; i <= end; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    listBox1.Items.Add(i);
+                }
+            }
+        }
+
+        private void btnPrimos_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+       
+
+            if (!int.TryParse(txtDesde.Text, out int start) || !int.TryParse(txtHasta.Text, out int end))
+            {
+                MessageBox.Show("Por favor, ingrese valores numéricos.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            for (int i = start; i <= end; i++)
+            {
+                if (IsPrime(i))
+                {
+                    listBox1.Items.Add(i);        
+                }
+            }
+        }
+        private bool IsPrime(int number)
+        {
+            if (number <= 1) return false;
+            if (number == 2) return true;
+            if (number % 2 == 0) return false;
+            for (int i = 3; i <= Math.Sqrt(number); i += 2)
+            {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
