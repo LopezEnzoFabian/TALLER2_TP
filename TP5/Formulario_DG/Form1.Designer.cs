f195f30d6893e5daff1efb943ec4652a9afc9c70
Enzo Fabian Lopez <enzolopez789@gmail.com>
2024-08-30 11:13:12 -0300

    tp 5 terminando

--- TP5/Formulario_DG/Form1.Designer.cs
+++ TP5/Formulario_DG/Form1.Designer.cs
@@ -29,47 +29,47 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dtFECHA = new System.Windows.Forms.DateTimePicker();
             this.btnAgregar = new System.Windows.Forms.Button();
             this.checkMujer = new System.Windows.Forms.CheckBox();
             this.checkHombre = new System.Windows.Forms.CheckBox();
             this.btnFoto = new System.Windows.Forms.Button();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtRUTA = new System.Windows.Forms.TextBox();
+            this.txtSALDO = new System.Windows.Forms.TextBox();
+            this.txtAPE = new System.Windows.Forms.TextBox();
+            this.txtNOM = new System.Windows.Forms.TextBox();
             this.lblSex = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.lblSaldo = new System.Windows.Forms.Label();
             this.lblApe = new System.Windows.Forms.Label();
             this.lblFec = new System.Windows.Forms.Label();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.dtg1 = new System.Windows.Forms.DataGridView();
+            this.imgFOTO = new System.Windows.Forms.PictureBox();
+            this.dgrid = new System.Windows.Forms.DataGridView();
             this.tAPE = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.tNOM = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.tFEC = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.tSEX = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.tDEL = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tDEL = new System.Windows.Forms.DataGridViewButtonColumn();
             this.tSAL = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.tFOT = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tFOT = new System.Windows.Forms.DataGridViewImageColumn();
             this.tRUT = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dtg1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgFOTO)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgrid)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.SystemColors.AppWorkspace;
-            this.panel1.Controls.Add(this.dateTimePicker1);
+            this.panel1.Controls.Add(this.dtFECHA);
             this.panel1.Controls.Add(this.btnAgregar);
             this.panel1.Controls.Add(this.checkMujer);
             this.panel1.Controls.Add(this.checkHombre);
             this.panel1.Controls.Add(this.btnFoto);
-            this.panel1.Controls.Add(this.textBox4);
-            this.panel1.Controls.Add(this.textBox3);
-            this.panel1.Controls.Add(this.textBox2);
-            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.txtRUTA);
+            this.panel1.Controls.Add(this.txtSALDO);
+            this.panel1.Controls.Add(this.txtAPE);
+            this.panel1.Controls.Add(this.txtNOM);
             this.panel1.Controls.Add(this.lblSex);
             this.panel1.Controls.Add(this.label1);
             this.panel1.Controls.Add(this.lblSaldo);
@@ -80,13 +80,13 @@
             this.panel1.Size = new System.Drawing.Size(297, 247);
             this.panel1.TabIndex = 0;
             // 
-            // dateTimePicker1
+            // dtFECHA
             // 
-            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.dateTimePicker1.Location = new System.Drawing.Point(126, 86);
-            this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(101, 20);
-            this.dateTimePicker1.TabIndex = 15;
+            this.dtFECHA.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtFECHA.Location = new System.Drawing.Point(126, 86);
+            this.dtFECHA.Name = "dtFECHA";
+            this.dtFECHA.Size = new System.Drawing.Size(101, 20);
+            this.dtFECHA.TabIndex = 15;
             // 
             // btnAgregar
             // 
@@ -96,6 +96,7 @@
             this.btnAgregar.TabIndex = 14;
             this.btnAgregar.Text = "Agregar";
             this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
             // 
             // checkMujer
             // 
@@ -125,35 +126,41 @@
             this.btnFoto.TabIndex = 11;
             this.btnFoto.Text = "Foto";
             this.btnFoto.UseVisualStyleBackColor = true;
+            this.btnFoto.Click += new System.EventHandler(this.btnFoto_Click);
             // 
-            // textBox4
+            // txtRUTA
             // 
-            this.textBox4.Location = new System.Drawing.Point(92, 161);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(126, 20);
-            this.textBox4.TabIndex = 10;
+            this.txtRUTA.Location = new System.Drawing.Point(92, 161);
+            this.txtRUTA.Name = "txtRUTA";
+            this.txtRUTA.Size = new System.Drawing.Size(126, 20);
+            this.txtRUTA.TabIndex = 10;
             // 
-            // textBox3
+            // txtSALDO
             // 
-            this.textBox3.Location = new System.Drawing.Point(92, 135);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(126, 20);
-            this.textBox3.TabIndex = 9;
+            this.txtSALDO.Location = new System.Drawing.Point(92, 135);
+            this.txtSALDO.Name = "txtSALDO";
+            this.txtSALDO.Size = new System.Drawing.Size(126, 20);
+            this.txtSALDO.TabIndex = 9;
+            this.txtSALDO.TextChanged += new System.EventHandler(this.txtSALDO_TextChanged);
+            this.txtSALDO.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtSALDO_KeyPress);
             // 
-            // textBox2
+            // txtAPE
             // 
-            this.textBox2.Location = new System.Drawing.Point(92, 51);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(126, 20);
-            this.textBox2.TabIndex = 8;
-            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            this.txtAPE.Location = new System.Drawing.Point(92, 51);
+            this.txtAPE.Name = "txtAPE";
+            this.txtAPE.Size = new System.Drawing.Size(126, 20);
+            this.txtAPE.TabIndex = 8;
+            this.txtAPE.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            this.txtAPE.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtAPE_KeyPress);
             // 
-            // textBox1
+            // txtNOM
             // 
-            this.textBox1.Location = new System.Drawing.Point(92, 18);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(126, 20);
-            this.textBox1.TabIndex = 7;
+            this.txtNOM.Location = new System.Drawing.Point(92, 18);
+            this.txtNOM.Name = "txtNOM";
+            this.txtNOM.Size = new System.Drawing.Size(126, 20);
+            this.txtNOM.TabIndex = 7;
+            this.txtNOM.TextChanged += new System.EventHandler(this.txtNOM_TextChanged);
+            this.txtNOM.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtNOM_KeyPress);
             // 
             // lblSex
             // 
@@ -200,22 +207,22 @@
             this.lblFec.TabIndex = 4;
             this.lblFec.Text = "Fecha de nacimiento";
             // 
-            // pictureBox1
+            // imgFOTO
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(322, 27);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(219, 247);
-            this.pictureBox1.TabIndex = 1;
-            this.pictureBox1.TabStop = false;
-            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.imgFOTO.Location = new System.Drawing.Point(322, 27);
+            this.imgFOTO.Name = "imgFOTO";
+            this.imgFOTO.Size = new System.Drawing.Size(219, 247);
+            this.imgFOTO.TabIndex = 1;
+            this.imgFOTO.TabStop = false;
+            this.imgFOTO.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
-            // dtg1
+            // dgrid
             // 
-            this.dtg1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
-            this.dtg1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedHeaders;
-            this.dtg1.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.None;
-            this.dtg1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dtg1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dgrid.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgrid.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedHeaders;
+            this.dgrid.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.None;
+            this.dgrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgrid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.tAPE,
             this.tNOM,
             this.tFEC,
@@ -224,11 +231,11 @@
             this.tSAL,
             this.tFOT,
             this.tRUT});
-            this.dtg1.Location = new System.Drawing.Point(28, 280);
-            this.dtg1.Name = "dtg1";
-            this.dtg1.Size = new System.Drawing.Size(513, 118);
-            this.dtg1.TabIndex = 2;
-            this.dtg1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick_1);
+            this.dgrid.Location = new System.Drawing.Point(28, 280);
+            this.dgrid.Name = "dgrid";
+            this.dgrid.Size = new System.Drawing.Size(513, 118);
+            this.dgrid.TabIndex = 2;
+            this.dgrid.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick_1);
             // 
             // tAPE
             // 
@@ -260,6 +267,8 @@
             this.tDEL.FillWeight = 56.62392F;
             this.tDEL.HeaderText = "Eliminar";
             this.tDEL.Name = "tDEL";
+            this.tDEL.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.tDEL.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
             // 
             // tSAL
             // 
@@ -272,6 +281,8 @@
             this.tFOT.FillWeight = 56.62392F;
             this.tFOT.HeaderText = "Foto";
             this.tFOT.Name = "tFOT";
+            this.tFOT.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.tFOT.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
             // 
             // tRUT
             // 
@@ -283,17 +294,17 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(582, 411);
-            this.Controls.Add(this.dtg1);
-            this.Controls.Add(this.pictureBox1);
+            this.ClientSize = new System.Drawing.Size(578, 411);
+            this.Controls.Add(this.dgrid);
+            this.Controls.Add(this.imgFOTO);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Formulario con DataGrid";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dtg1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgFOTO)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgrid)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -301,11 +312,11 @@
         #endregion
 
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox imgFOTO;
+        private System.Windows.Forms.TextBox txtRUTA;
+        private System.Windows.Forms.TextBox txtSALDO;
+        private System.Windows.Forms.TextBox txtAPE;
+        private System.Windows.Forms.TextBox txtNOM;
         private System.Windows.Forms.Label lblSex;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label lblSaldo;
@@ -315,15 +326,15 @@
         private System.Windows.Forms.CheckBox checkMujer;
         private System.Windows.Forms.CheckBox checkHombre;
         private System.Windows.Forms.Button btnAgregar;
-        private System.Windows.Forms.DateTimePicker dateTimePicker1;
-        private System.Windows.Forms.DataGridView dtg1;
+        private System.Windows.Forms.DateTimePicker dtFECHA;
+        private System.Windows.Forms.DataGridView dgrid;
         private System.Windows.Forms.DataGridViewTextBoxColumn tAPE;
         private System.Windows.Forms.DataGridViewTextBoxColumn tNOM;
         private System.Windows.Forms.DataGridViewTextBoxColumn tFEC;
         private System.Windows.Forms.DataGridViewTextBoxColumn tSEX;
-        private System.Windows.Forms.DataGridViewTextBoxColumn tDEL;
+        private System.Windows.Forms.DataGridViewButtonColumn tDEL;
         private System.Windows.Forms.DataGridViewTextBoxColumn tSAL;
-        private System.Windows.Forms.DataGridViewTextBoxColumn tFOT;
+        private System.Windows.Forms.DataGridViewImageColumn tFOT;
         private System.Windows.Forms.DataGridViewTextBoxColumn tRUT;
     }
 }
--- TP5/Formulario_DG/Form1.cs
+++ TP5/Formulario_DG/Form1.cs
@@ -29,7 +29,7 @@ namespace Formulario_DG
 
         private void textBox2_TextChanged(object sender, EventArgs e)
         {
-
+         
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -53,6 +53,73 @@ namespace Formulario_DG
         }
 
         private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnFoto_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtNOM_TextChanged(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void txtNOM_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsLetter(e.KeyChar) && e.KeyChar != (char)Keys.Back)
+            {
+                e.Handled = true; // Evitar que el carácter se introduzca en el TextBox
+            }
+        }
+
+        private void txtAPE_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsLetter(e.KeyChar) && e.KeyChar != (char)Keys.Back)
+            {
+                e.Handled = true; // Evitar que el carácter se introduzca en el TextBox
+            }
+        }
+
+        private void txtSALDO_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtSALDO_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && e.KeyChar != (char)Keys.Back)
+            {
+                e.Handled = true; // Evitar que el carácter se introduzca en el TextBox
+            }
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            int fila = dgrid.Rows.Add();
+
+
+            dgrid.Rows[fila].Cells[0].Value = txtNOM.Text;
+            dgrid.Rows[fila].Cells[1].Value = txtAPE.Text;
+            dgrid.Rows[fila].Cells[2].Value = dtFECHA.Value;
+            if (checkHombre.Checked)
+            {
+                dgrid.Rows[fila].Cells[3].Value = checkHombre.Text;
+                checkHombre.Checked = false;
+            }
+            else {
+                if(checkMujer.Checked)
+                dgrid.Rows[fila].Cells[3].Value = checkMujer.Text;
+                checkMujer.Checked = false;
+            }
+           
+
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
 
         }
